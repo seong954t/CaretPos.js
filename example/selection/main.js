d85c58b0be9cb10e57c7c233644a237709b05e48
Seoungtae Kim <seomtae@naver.com>
2019-06-04 14:21:54 +0900

    첫 줄 div or p 태그 추가 기능 추가, ie브라우저 확인 및 anchor / focus 기능 추가

--- example/selection/main.js
+++ example/selection/main.js
@@ -1,27 +1,61 @@
 window.onload = function(){
+    addDIVEmptyFirstLine();
     const editor = document.querySelector('div[contenteditable=true]');
     editor.addEventListener('input', setInfo);
+    editor.addEventListener('keydown', addDIVEmptyFirstLine);
+    editor.addEventListener('keyup', addDIVEmptyFirstLine);
     document.addEventListener('selectionchange', setInfo);
 }
 
 function setInfo(){
     const selection = window.getSelection();
     setBaseInfo(selection);
+    setAnchorInfo(selection);
     setExtentInfo(selection);
+    setFocusInfo(selection);
     setSelectionInfo(selection);
 }
 
 function setBaseInfo(selection){
-    document.querySelector('#base-text').innerText = selection.anchorNode.textContent;
-    document.querySelector('#base-offset').innerText = selection.anchorOffset;
+    if(!isIEBrowser()){
+        document.querySelector('#base-text').innerText = selection.baseNode.textContent;
+        document.querySelector('#base-offset').innerText = selection.baseOffset;
+    }
+}
+
+function setAnchorInfo(selection){
+    document.querySelector('#anchor-text').innerText = selection.anchorNode.textContent;
+    document.querySelector('#anchor-offset').innerText = selection.anchorOffset;
 }
 
 function setExtentInfo(selection){
-    document.querySelector('#extent-text').innerText = selection.focusNode.textContent;
-    document.querySelector('#extent-offset').innerText = selection.focusOffset;
+    if(!isIEBrowser()){
+        document.querySelector('#extent-text').innerText = selection.extentNode.textContent;
+        document.querySelector('#extent-offset').innerText = selection.extentOffset;
+    }
+}
+
+function setFocusInfo(selection){
+    document.querySelector('#focus-text').innerText = selection.focusNode.textContent;
+    document.querySelector('#focus-offset').innerText = selection.focusOffset;
 }
 
 function setSelectionInfo(selection){
     document.querySelector('#selection-text').innerText = selection.toString();
     document.querySelector('#selection-offset').innerText = selection.anchorOffset + ' ~ ' + selection.focusOffset;
+}
+
+function addDIVEmptyFirstLine(){
+    const editor = document.querySelector('div[contenteditable=true]');
+    if(editor.innerHTML === '' || document.querySelector('div[contenteditable=true]').childNodes[0].tagName === 'BR'){
+        if(isIEBrowser()){
+            editor.innerHTML = '<p></p>';
+        }else{
+            editor.innerHTML = '<div></div>';
+        }
+    }
+}
+
+function isIEBrowser(){
+    return ((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (navigator.userAgent.toLowerCase().indexOf("msie") != -1));
 }
