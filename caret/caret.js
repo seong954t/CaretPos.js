d0149193ebe3983c34433c5b293c736de185d49b
Seoungtae Kim <seomtae@naver.com>
2019-06-14 11:54:34 +0900

    caret.js 라이브러르 skeleton코드 업로드

---
+++ caret/caret.js
@@ -0,0 +1,77 @@
+/**
+ * Hangul.js
+ * https://github.com/seong954t/CaretPos.js
+ *
+ * Copyright 2019, Seungtae Kim
+ * under the MIT license.
+ */
+(function() {
+    /**
+     * caret position setting functions
+     * you can get/set caret information with text index
+    */
+    let positionEditor;
+
+    function caretInitialize(element){
+        positionEditor = element;
+    }
+
+    function getStartAndEndCaretPositions(){
+
+    }
+
+    function setStartAndEndCaretPositions(start, end){
+
+    }
+
+    const caretPosition = {
+        initialize: caretInitialize,
+        getCaretPositions: getStartAndEndCaretPositions,
+        setCaretPositions: setStartAndEndCaretPositions
+    }
+
+    /**
+     * caret visualization setting functions
+     *  you can draw customize caret with text index
+    */ 
+    let visualizationEditor;
+
+    function visualizationInitialize(element){
+        visualizationEditor = element;
+    }
+
+    function createVisualizationCaret(){
+
+    }
+
+    function removeVisualizationCaret(){
+
+    }
+
+    function drawVisualizationCaret(){
+
+    }
+
+    const visualization = {
+        initialize: visualizationInitialize,
+        createCaret: createVisualizationCaret,
+        removeCaret: removeVisualizationCaret,
+        drawCaret: drawVisualizationCaret
+    }
+
+    const caret = {
+        initialize: initialize,
+        position: caretPosition,
+        visualization: visualization
+    };
+
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return caret;
+        });
+    } else if (typeof module !== 'undefined') {
+        module.exports = caret;
+    } else {
+        window.caret = caret;
+    }
+})();
